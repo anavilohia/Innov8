d020feead62223b3f46308a3030639a901d5cb64
Anavi Lohia <anavi.lohia@columbia.edu>
2024-10-10 06:17:59 -0400

    Project skeleton

---
+++ LiveSched/src/main/java/dev/coms4156/project/LiveSched/LiveSchedApplication.java
@@ -0,0 +1,38 @@
+package dev.coms4156.project.livesched;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class contains all the startup logic for the application.
+ */
+@SpringBootApplication
+public class LiveSchedApplication implements CommandLineRunner {
+
+  /**
+   * The main launcher for the services all it does
+   * is make a call to the overridden run method.
+   *
+   * @param args A {@code String[]} of any potential
+   *             runtime arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(LiveSchedApplication.class, args);
+  }
+
+  /**
+   * This contains all the setup logic, it will mainly be focused
+   * on loading up and creating an instance of the database based
+   * off a saved file or will create a fresh database if the file
+   * is not present.
+   *
+   * @param args A {@code String[]} of any potential runtime args
+   */
+  @Override
+  public void run(String[] args) {
+    //to be added
+    return;
+  }
+
+}
