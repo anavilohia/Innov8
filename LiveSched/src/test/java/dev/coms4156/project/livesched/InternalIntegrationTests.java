967910dffca68ad22e3e229600a69b37ad6e86c1
Jonghyun (Joann) Lee <151266186+jonghyun-joann-lee@users.noreply.github.com>
2024-12-06 01:58:05 -0500

    Added external integration tests

--- LiveSched/src/test/java/dev/coms4156/project/livesched/InternalIntegrationTests.java
+++ LiveSched/src/test/java/dev/coms4156/project/livesched/InternalIntegrationTests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
